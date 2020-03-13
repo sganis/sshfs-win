ea3f66f5dd7ac0ebe3e80b3080dfa259ae866d24
San <sganis@gmail.com>
2020-03-13 16:09:59 +0300

    Removed openssh tools

--- ci/testfs.bat
+++ ci/testfs.bat
@@ -8,25 +8,6 @@ setlocal EnableDelayedExpansion
 set DIR=%~dp0
 set DIR=%DIR:~0,-1%
 
-:: %DIR%\winfsp-tests-x64.exe --external --resilient --case-insensitive-cmp ^
-:: 	-create_allocation_test ^
-:: 	-create_fileattr_test ^
-:: 	-create_notraverse_test ^
-:: 	-create_backup_test ^
-:: 	-create_restore_test ^
-:: 	-create_namelen_test ^
-:: 	-getfileinfo_name_test ^
-:: 	-setfileinfo_test ^
-:: 	-delete_access_test ^
-:: 	-delete_mmap_test ^
-:: 	-rename_flipflop_test ^
-:: 	-rename_mmap_test ^
-:: 	-setsecurity_test ^
-:: 	-querydir_namelen_test ^
-:: 	-exec_rename_dir_test ^
-:: 	-reparse* -stream*
-:: if !ERRORLEVEL! neq 0 goto fail
-
 echo > test
 %DIR%\fsx.exe -N 5000 -L test xxxxxx
 del test test.*
