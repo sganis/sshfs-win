9bb2b9b42b240f206f7ec77f5c4a799c00d57f42
San <sganis@gmail.com>
2020-03-14 08:54:52 +0300

    CI update

--- ci/testfs.bat
+++ ci/testfs.bat
@@ -9,12 +9,12 @@ set DIR=%~dp0
 set DIR=%DIR:~0,-1%
 
 echo > test
-%DIR%\fsx.exe -N 5000 -L test xxxxxx
+%DIR%\fsx.exe -N 5000 test xxxxxx
 del test test.*
 if !ERRORLEVEL! neq 0 goto fail
 
-%DIR%\fsbench-x64.exe -rdwr_cc_* -mmap_* ^
-	-file_attr* -file_list_single* -file_list_none* -rdwr_nc_*
+%DIR%\fsbench-x64.exe -rdwr_cc_* -mmap_* 
+:: -file_attr* -file_list_single* -file_list_none* -rdwr_nc_*
 if !ERRORLEVEL! neq 0 goto fail
 
 echo PASSED
